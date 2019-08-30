7454b321701daac151402213313c88af3132dfd0
unknown <amedina@growthaccelerationpartners.com>
2019-08-30 16:00:12 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserBarcodeReader } from '@zxing/library'
+import image from './code-4.jpeg'
+
+const init = async () => {
+  const codeReader = new BrowserBarcodeReader()
+  const img = document.getElementById('img')
+
+  console.log(img)
+
+  try {
+    const result = await codeReader.decodeFromImage(img)
+    console.log(result)
+  } catch (err) {
+    console.error(err)
+  }
 }
 
-export default App;
+function App() {
+  useEffect(() => {
+    init()
+  }, [])
+  return (
+    <div className="App">
+      <img id="img" src={image} />
+    </div>
+  )
+}
+
+export default App
